380afda1b5b3c71a5e572279c7e0cd72ef0a01e1
Hydrong <chickchok06@gmail.com>
2025-07-04 16:57:25 +0900

    ch

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState, forwardRef } from "react";
 import styled from "styled-components";
 import { uuid, type UUID } from "../utils/uuid";
+import { noteNames } from "../constants/noteNames";
+import { pitches } from "../constants/pitches";
 
 const CntStyle = styled.div`
   padding: 70px;
@@ -49,13 +51,16 @@ const PinBarStyle = styled.div<{ value: number }>`
   bottom: ${(props) => props.value}px;
 `;
 
+// 보드 타입
 interface Board {
   id: UUID;
   pins: Pin[];
 }
-
+// 핀 타입
 interface Pin {
   id: UUID;
+  boardId: UUID;
+  exist: boolean;
   noteName: string;
   pitch: number;
   duration: number;
@@ -63,35 +68,40 @@ interface Pin {
 }
 
 function noteNameToPitch(noteName: string) {
-  const values: string[] = [];
-  const pitches: number[] = [];
-  return pitches[values.indexOf(noteName)];
+  return pitches[noteNames.indexOf(noteName)];
 }
 
 function pitchToNoteName(pitch: number) {
-  const values: string[] = [];
-  const pitches: number[] = [1, 2, 3, 4, 5];
-  return values[pitches.indexOf(pitch)];
+  return noteNames[pitches.indexOf(pitch)];
 }
 
 function getNextPitch(pitch: number) {
-  const pitches: number[] = [1, 2, 3, 4, 5];
-  return pitches[pitches.indexOf(pitch) + 1];
+  return pitches[pitches.indexOf(pitch) + 1] ?? pitches[pitches.length - 1];
 }
 function getPreviousPitch(pitch: number) {
-  const pitches: number[] = [1, 2, 3, 4, 5];
-  return pitches[pitches.indexOf(pitch) + 1];
+  return pitches[pitches.indexOf(pitch) - 1] ?? pitches[0];
 }
 
 // || --------- 핀 컴포넌트 --------- ||
-const PinComp: React.FC<{ pin: Pin; onFocus: (pin: Pin) => void }> = ({
-  pin,
-  onFocus,
-}) => {
+const PinComp: React.FC<{
+  noteName: string;
+  pitch: number;
+  onFocus: () => void;
+}> = ({ noteName, pitch, onFocus }) => {
   return (
-    <PinWrpStyle onFocus={() => onFocus(pin)}>
-      <PinStyle tabIndex={0}>{pin.noteName}</PinStyle>
-      <PinBarStyle value={pin.pitch} />
+    <PinWrpStyle onFocus={onFocus}>
+      <PinStyle tabIndex={0}>{noteName}</PinStyle>
+      <PinBarStyle value={pitch} />
+    </PinWrpStyle>
+  );
+};
+// || -------- 빈 핀 컴포넌트 -------- ||
+const EmptyPinComp: React.FC<{
+  onFocus: () => void;
+}> = ({ onFocus }) => {
+  return (
+    <PinWrpStyle onFocus={onFocus}>
+      <PinStyle tabIndex={0}></PinStyle>
     </PinWrpStyle>
   );
 };
@@ -99,27 +109,63 @@ const PinComp: React.FC<{ pin: Pin; onFocus: (pin: Pin) => void }> = ({
 // || -------- 메인 컴포넌트 -------- ||
 export default function Content() {
   const [pinCount, setPinCount] = useState<number>(10);
-  const [boards, setBoards] = useState<Board[]>([
-    {
-      id: uuid(),
-      pins: [
-        { ...p1, id: uuid() },
-        { ...p1, id: uuid() },
-        { ...p1, id: uuid() },
-        { ...p1, id: uuid() },
-      ],
-    },
-  ]);
-  const [focusedPin, setFocusedPin] = useState<Pin | null>(null);
+  // 전체 Board 상태 ( 속성으로 Pin 포함 )
+  const [boards, setBoards] = useState<Board[]>([]);
+  const pinRefs = useRef<Record<UUID, HTMLDivElement | null>>({}); // 핀 참조 배열
+  const focusedPinRef = useRef<Pin | null>(null); // 현재 포커스된 핀 참조
+
+  // 새 빈 보드 생성
+  function createBoard() {
+    const boardId = uuid()
+    const board: Board = {
+      id: boardId,
+      pins: [{id:uuid(),boardId,duration:1,exist:true,noteName:"C4",pitch:1,startTime:0},{id:uuid(),boardId,duration:1,exist:true,noteName:"C4",pitch:1,startTime:0}],
+    };
+    setBoards([...boards, board]);
+    return board;
+  }
+  // 보드 길이 수정
+  function setBoardSize(board: Board, size: number) {
+    const copy = [...boards];
+    const idx = copy.indexOf(board);
+    const newPins = board.pins;
+    while (newPins.length < size) {
+      newPins.push({
+        id: uuid(),
+        boardId: board.id,
+        duration: 1,
+        exist: true,
+        noteName: "C4",
+        pitch: 0,
+        startTime: 1,
+      });
+    }
+    const newBoard: Board = { id: board.id, pins: newPins };
+    copy[idx] = board;
+    setBoards(copy);
+  }
+  // 핀 변경
+  function setPin(pin: Pin) {
+    const targetBoard = boards.filter((board) => board.pins.includes(pin))[0];
+    const boardIdx = boards.indexOf(targetBoard);
+    const idx = targetBoard.pins.indexOf(pin);
+    targetBoard.pins[idx] = pin;
+    boards[boardIdx] = targetBoard;
+  }
 
   useEffect(() => {
+    const board = createBoard();
+    // setTimeout(()=>{setBoardSize(board, 5)},100)
+    ;
     const handleWheel = (e: WheelEvent) => {
-      if (focusedPin && e.deltaY !== 0) {
-        console.log("wheel");
-        focusedPin.pitch =
-          e.deltaY > 0
-            ? getNextPitch(focusedPin.pitch)
-            : getPreviousPitch(focusedPin.pitch);
+      if (focusedPinRef.current && e.deltaY !== 0) {
+        const now = { ...focusedPinRef.current };
+        now.pitch =
+          e.deltaY > 0 ? getNextPitch(now.pitch) : getPreviousPitch(now.pitch);
+        const copy = [...boards];
+        copy[0];
+        setBoards((prev) => [...prev]);
+        console.log(now.pitch);
       }
     };
     window.addEventListener("wheel", handleWheel);
@@ -128,24 +174,42 @@ export default function Content() {
     };
   }, []);
 
-  const boardElements = boards.map((board) => (
-    <BoardStyle key={board.id}>
-      {board.pins.map((pin) => (
-        <PinComp
-          onFocus={(pin) => {
-            setFocusedPin(pin);
-          }}
-          pin={pin}
-          key={pin.id}
-        />
-      ))}
-    </BoardStyle>
-  ));
-
   // || --------- DOM ---------- ||
   return (
     <CntStyle>
-      <BoardWrpStyle>{boardElements}</BoardWrpStyle>
+      <BoardWrpStyle>
+        {boards.map((board) => (
+          <BoardStyle key={board.id}>
+            {board.pins.map((pin) =>
+              pin.exist ? (
+                <PinComp
+                  onFocus={() => {
+                    console.log(pin);
+                    focusedPinRef.current = pin;
+                  }}
+                  noteName={pin.noteName}
+                  pitch={pin.pitch}
+                  key={pin.id}
+                />
+              ) : (
+                <EmptyPinComp
+                  onFocus={() => {
+                    setPin({
+                      id: pin.id,
+                      boardId: pin.boardId,
+                      duration: 1,
+                      exist: true,
+                      noteName: "C4",
+                      pitch: 1,
+                      startTime: 1,
+                    });
+                  }}
+                />
+              )
+            )}
+          </BoardStyle>
+        ))}
+      </BoardWrpStyle>
     </CntStyle>
   );
 }
